d5d33bd42ca40ee360a749203de9f8660c43e839
unknown <hohoaisan@gmail.com>
2021-12-03 10:03:17 +0700

    format service

---
+++ src/services/format.service.js
@@ -0,0 +1,84 @@
+const httpStatus = require('http-status');
+const { Op } = require('sequelize');
+const { Format } = require('../models');
+const ApiError = require('../utils/ApiError');
+/**
+ * Get format by id
+ * @param {ObjectId} id
+ * @returns {Promise<Format>}
+ */
+const getFormatById = async (id, options) => {
+  return Format.findByPk(id, options);
+};
+
+/**
+ * Get format by id
+ * @param {ObjectId} key
+ * @returns {Promise<Format>}
+ */
+const getFormatByKey = async (key, options = {}) => {
+  const { excludeKey, ...restOptions } = options;
+  if (excludeKey) {
+    return Format.findOne({ where: { key: { [Op.eq]: key, [Op.not]: excludeKey } }, ...options });
+  }
+  return Format.findOne({ where: { key }, ...restOptions });
+};
+
+/**
+ * Create a format
+ * @param {Object} formatBody
+ * @returns {Promise<Format>}
+ */
+
+const createFormat = async (formatBody) => {
+  const format = await Format.create(formatBody);
+  return format.toJSON();
+};
+
+/**
+ * Query for formats
+ * @returns {Promise<QueryResult>}
+ */
+const queryFormats = async (options) => {
+  const formats = await Format.findAll(options);
+  return formats;
+};
+
+/**
+ * Update format by id
+ * @param {ObjectId} formatId
+ * @param {Object} updateBody
+ * @returns {Promise<Format>}
+ */
+const updateFormatById = async (formatId, updateBody) => {
+  const format = await getFormatById(formatId);
+  if (!format) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Format not found or had been deleted');
+  }
+  format.set(updateBody);
+  await format.save();
+  return format;
+};
+
+/**
+ * Delete format by id
+ * @param {ObjectId} formatId
+ * @returns {Promise<Format>}
+ */
+const deleteFormatById = async (formatId) => {
+  const format = await getFormatById(formatId);
+  if (!format) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Format not found or had been deleted');
+  }
+  await format.destroy();
+  return format;
+};
+
+module.exports = {
+  createFormat,
+  queryFormats,
+  getFormatById,
+  getFormatByKey,
+  updateFormatById,
+  deleteFormatById,
+};
