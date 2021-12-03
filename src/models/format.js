7ba141b44035b2315dd7511949f76efe1ba8ac7d
unknown <hohoaisan@gmail.com>
2021-12-03 10:01:52 +0700

    format model

--- src/models/format.js
+++ src/models/format.js
@@ -10,25 +10,44 @@ class format extends Sequelize.Model {
           defaultValue: DataTypes.UUIDV4,
           primaryKey: true,
         },
+        key: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
         name: {
           type: DataTypes.STRING,
           allowNull: false,
+        },
+        description: {
+          type: DataTypes.STRING,
+          allowNull: true,
         },
       },
       {
         sequelize,
         tableName: 'format',
         schema: 'public',
-        timestamps: false,
+        timestamps: true,
         indexes: [
           {
             name: 'format_pkey',
             unique: true,
             fields: [{ name: 'id' }],
           },
+          {
+            unique: true,
+            fields: ['key'],
+          },
         ],
       }
     );
+    super.beforeSave((f) => {
+      if (f.key) {
+        // eslint-disable-next-line no-param-reassign
+        f.key = f.key.toLowerCase();
+      }
+    });
     return format;
   }
 }
